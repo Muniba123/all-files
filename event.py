9df44251767c2eef95597aa50b2eaad4527f4247
Muniba123 <50288566+Muniba123@users.noreply.github.com>
2019-07-07 18:00:02 +0500

    old assignment

---
+++ event.py
@@ -0,0 +1,10 @@
+event_venue = input('Where is event?')
+if event_venue == 'phopo Home':
+    print('Iam busy can not go!')
+elif event_venue == 'Mamoo Home':
+    print('OK i am coming!')
+elif event_venue == 'chacha Home':
+    print('I am coming i am missing chacha')
+else:
+     print('surely i will go how can i miss a party!')
+
